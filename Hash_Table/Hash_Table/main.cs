0cd90e40e93a2ab717590eb4841f177069644ab5
rajshri-priya <rajshri.priya1708@.gmail.com>
2022-11-18 18:45:25 +0530

    hash table included

---
+++ Hash_Table/Hash_Table/main.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hash_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Hash Table");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+            string hash5 = hash.Get("5");
+            Console.WriteLine("5th index value:" + hash5);
+            string hash2 = hash.Get("2");
+            Console.WriteLine("2nd index value:" + hash2);
+
+
+
+        }
+    }
+}
+
